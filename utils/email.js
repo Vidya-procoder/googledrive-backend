9b63960799d997daa59f5b59c9c5bd6ae5393752
Vidya-procoder <vidyasridhanasekaran@gmail.com>
2026-01-31 23:09:02 +0530

    fix: switch to port 587 for email to fix timeout issues

--- utils/email.js
+++ utils/email.js
@@ -2,10 +2,12 @@ const nodemailer = require('nodemailer');
 
 // Create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD // This must be an App Password, not your login password
+    pass: process.env.EMAIL_PASSWORD
   }
 });
 
