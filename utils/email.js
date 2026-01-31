07045012fd1c34d19ed3b792540d9021f26d48db
Vidya-procoder <vidyasridhanasekaran@gmail.com>
2026-01-31 23:28:58 +0530

    mail issue - 31-01-2026

--- utils/email.js
+++ utils/email.js
@@ -12,7 +12,11 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
-  }
+  },
+  logger: true, // Log to console
+  debug: true, // Include debug info
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 5000 // 5 seconds
 });
 
 // Helper to send email
