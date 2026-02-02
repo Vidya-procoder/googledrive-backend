045c381f8d5ad05ed042262b9824de04fdcb596b
Vidya-procoder <vidyasridhanasekaran@gmail.com>
2026-02-02 18:06:27 +0530

    port change

--- utils/email.js
+++ utils/email.js
@@ -5,10 +5,11 @@ const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
 
 // Create reusable transporter object using the default SMTP transport
+// Create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
